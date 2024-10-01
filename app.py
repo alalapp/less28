6853883d1540a3ca7af9fcedfa5493342732b468
alalapp <32865252+alalapp@users.noreply.github.com>
2024-10-01 16:52:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def escape_special_characters(text):
 
 def get_recent_news(topic):
     url = f"https://newsapi.org/v2/everything?q={topic}&apiKey=73db81ae9b614a06b3badcab2c1cd513"
-    response = requests.get(urll, timeout=60))
+    response = requests.get(url, timeout=60))
     articles = response.json().get("articles", [])
     recent_news = [escape_special_characters(article["title"]) for article in articles[:3]]
     return "\n".join(recent_news)
